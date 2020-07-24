90506335f8450b618580e5f9110e34900a0248cb
TJ McKinley <t.mckinley@exeter.ac.uk>
2020-07-24 13:18:55 +0100

    Started to add versioning dropdown box.

---
+++ docs/_version.js
@@ -0,0 +1,5 @@
+document.writeln("<form>");
+document.writeln("<select name=\"version\" id=\"version\" onchange=\"goToVersion()\">");
+document.writeln("<option value=\"../docs_dropdown/index.html\">dropdown</option>");
+document.writeln("</select>");
+document.writeln("</form>");
