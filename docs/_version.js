423c3618db2066cb368d4ec56697c51f380ad5b1
TJ McKinley <t.mckinley@exeter.ac.uk>
2020-08-04 21:56:20 +0100

    Fixed errors with dropdown boxes. Added simple Python server for testing.

--- docs/_version.js
+++
@@ -1,5 +0,0 @@
-document.writeln("<form>");
-document.writeln("<select name=\"version\" id=\"version\" onchange=\"goToVersion()\">");
-document.writeln("<option value=\"../docs_dropdown/index.html\">dropdown</option>");
-document.writeln("</select>");
-document.writeln("</form>");
